f3cb2694ac28cfb7f424b38dcc911c9830607880
nonewsman <62964756+nonewsman@users.noreply.github.com>
2023-10-15 12:47:33 -0400

    changes for local
    changes for local

--- om-buffer-expert/src/app/solution.service.ts
+++ om-buffer-expert/src/app/solution.service.ts
@@ -35,9 +35,9 @@ export class SolutionService {
   solutionAdded: EventEmitter<void> = new EventEmitter<void>();
   solutionEdited: EventEmitter<void> = new EventEmitter<void>();
 
- private apiUrl = 'https://buffer-designer-service-zuynyusrbq-uc.a.run.app/api'; // Replace with your Flask API URL
+ //private apiUrl = 'https://buffer-designer-service-zuynyusrbq-uc.a.run.app/api'; // Replace with your Flask API URL
   
-  //private apiUrl = 'http://127.0.0.1:5000/api'; // Replace with your Flask API URL
+  private apiUrl = 'http://127.0.0.1:5000/api'; // Replace with your Flask API URL
 
   constructor(private http: HttpClient) {
     this.fetchCompoundFunDict();
