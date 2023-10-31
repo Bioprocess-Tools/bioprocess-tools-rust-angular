83147250da63b8efe102b2ce5bacb2ec5566e99c
nonewsman <62964756+nonewsman@users.noreply.github.com>
2023-10-30 21:30:27 -0400

    correct image paths

--- om-buffer-expert/src/app/static-pages/about-us/about-us.component.ts
+++ om-buffer-expert/src/app/static-pages/about-us/about-us.component.ts
@@ -12,19 +12,19 @@ export class AboutUsComponent implements OnInit {
       name: 'Natraj Ram., Ph.D.',
       title: 'Technical Advisor',
       bio: 'Passionate engineer with over 30 years of experience in biotech, with deep interest in democratizing knowledge',
-      image: '/assets/images/NatrajSmallOM.JPG' // place actual path to image
+      image: '/assets/images/Natrajtiny.JPG' // place actual path to image
     },
     {
       name: 'Adharsh Ravi',
       title: 'Developer',
       bio: 'Enthusiastic, diligent, cerebral techie - who loves problem solving',
-      image: '/assets/images/adharsh linked in.jpg' // place actual path to image
+      image: '/assets/images/adharshtiny.jpg' // place actual path to image
     },
     {
       name: 'Srividhya Ram',
       title: 'Design Consultant and Project Coordinator',
       bio: 'Naturally talented design thinker, with the ability manage people and projects with inspiration and discipline!',
-      image: '/assets/images/Srividhya_Ram_Small.JPG' // place actual path to image
+      image: '/assets/images/Vidhya.JPG' // place actual path to image
     }
     // Add more team members as needed
   ];
