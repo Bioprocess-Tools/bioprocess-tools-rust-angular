62fbfce24cb1f577fd0bf38cc22207e6785e68c9
natraj-ram <84460074+natraj-ram@users.noreply.github.com>
2024-04-01 22:01:11 -0400

    color add

--- om-buffer-expert/src/app/solution-mixture.service.ts
+++ om-buffer-expert/src/app/solution-mixture.service.ts
@@ -28,6 +28,16 @@ export class SolutionMixtureService {
   StepsSubject = new BehaviorSubject<Step[]>([]);
   Steps$ = this.StepsSubject.asObservable();
   allPlotsData: any[] = [];
+  //phase_colors: string[] = ['#FF0000', '#FFA500', '#FFFF00', '#008000', '#0000FF', '#4B0082', '#EE82EE', '#000000', '#A9A9A9', '#FFFFFF'];
+  //solution_colors:string[] = ['#FF0000', '#FFA500', '#FFFF00', '#008000', '#0000FF', '#4B0082', '#EE82EE', '#000000', '#A9A9A9', '#FFFFFF'];
+  compound_colors:string[] = ['#ff8fab', '#a7bed3', '#ffcaaf', '#e5c2e4', '#eebab2', '#b8e0d2', '#ff9770', '#ccd4bf', '#e8d1c5', '#98aab3','#ecdc93', '#a1e7e8'];  
+  
+  
+  
+  ion_colors:string[] = ['#832c76', '#4F3F84', '#BB3630', '#d0b285', '#00a1d3', '#929580', '#3d5859', '#769f52']; 
+  solution_colors:string[]=['#3e94e5','#c54b6c','#37667e', '#dc828f','#d29f8c', '#a15d98', '#8c7386', '#218b82', '#f27348', '#9c9359', '#874741', '#40393e', '#f4c815', '#2cced2']
+
+  
   constructor(private http: HttpClient) {
     
     
